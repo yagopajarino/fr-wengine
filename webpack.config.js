233febb2866a3298d12236489942aa4fa16957d4
Yago Pajariño <yagopajarino@outlook.com>
2022-07-01 10:37:18 -0300

    Add new footer

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'scripts.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "scripts.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  mode:"development",
- module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-     {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+  mode: "development",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-   ],
- },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
