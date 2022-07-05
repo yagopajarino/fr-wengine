94f1d5a69b4a5cef12cb9d57d7713bc68bfe32c5
Yago Pajariño <yagopajarino@outlook.com>
2022-07-05 00:59:57 -0300

    Add comments section

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./style/servicios.css";
 import "./style/promociones.css";
 import "./style/contacto.css";
 import "./style/efectos.css";
+import "./style/comments.css";
 import imgLogo from "./imgHandlers/img_logo";
 import imgServicios from "./imgHandlers/img_servicios";
 import imgPromociones from "./imgHandlers/img_promociones";
@@ -12,7 +13,7 @@ import imgRedes from "./imgHandlers/img_redes";
 import imgIcon from "./imgHandlers/img_icon";
 import imgQuienSomos from "./imgHandlers/img_quieneSomos";
 import imgArrow from "./imgHandlers/img_arrow";
-import showComments from "./comments/showComments";
+import getCommentsDivs from "./comments/getCommentsDivs";
 import dropdownMenu from "./components/dropdown";
 import { showArrow, goToTop } from "./components/scrolling";
 
@@ -35,4 +36,24 @@ window.onscroll = () => {
   showArrow(mybutton);
 };
 
-console.log(showComments());
+const comments = getCommentsDivs();
+const commentsContainer = document.querySelector(".comGrid");
+// const p = document.createElement("p");
+// p.textContent = "Esto se anima";
+// commentsContainer.appendChild(p);
+commentsContainer.appendChild(comments[0]);
+const WORDS_PER_MILISECOND = (50 / 60) * 1000;
+let i = 1;
+setInterval(() => {
+  const child = commentsContainer.querySelector(".comment");
+  if (child) {
+    commentsContainer.removeChild(child);
+  }
+  commentsContainer.appendChild(comments[i]);
+  i = (i + 1) % comments.length;
+  // if (p.textContent == "Esto se anima") {
+  //   p.textContent = "Hola que tal";
+  // } else {
+  //   p.textContent = "Esto se anima";
+  // }
+}, WORDS_PER_MILISECOND * comments[i].querySelector("p").textContent.split(" ").length);
